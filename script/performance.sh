11b0bf9d837c46095bde4fd84b1565605a06846b
tanzaku <ume.drink@gmail.com>
2019-10-13 21:48:58 +0900

    profile

--- script/performance.sh
+++ script/performance.sh
@@ -2,8 +2,20 @@
 
 set -eu
 
-g++ JumpAround.cpp -pg -O3 -std=c++11 -Wall -Wextra -Wshadow -DADJUST
 
+### using gprof
+# g++ JumpAround.cpp -pg -O3 -std=c++11 -Wall -Wextra -Wshadow -DADJUST
+# java -jar tester.jar -exec "./a.out" -seed 2 -novis
+# gprof a.out gmon.out > gmon.txt
+
+### using gperftools
+# brew install gperftools
+# brew install graphviz
+
+export CPUPROFILE=prof.out
+export CPUPROFILE_FREQUENCY=100000
+g++ JumpAround.cpp -O3 -std=c++11 -Wall -Wextra -Wshadow -DPROFILE -DADJUST -lprofiler -Wl,-no_pie
 java -jar tester.jar -exec "./a.out" -seed 2 -novis
-
-gprof a.out gmon.out > gmon.txt
+# otool -L ./a.out
+pprof --text ./a.out $CPUPROFILE > prof.txt
+pprof --dot ./a.out $CPUPROFILE > prof.dot && dot -T png prof.dot > prof.png
---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#ifdef PROFILE
+#include <gperftools/profiler.h>
+#endif
+
+using namespace std;
+
+int main()
+{
+#ifdef PROFILE
+  ProfilerStart("");
+#endif
+
+
+#ifdef PROFILE
+  ProfilerStop();
+#endif
+}
